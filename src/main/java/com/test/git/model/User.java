dbc11e938b6945616978ac086497fc5fca78728a
jsjune <avraskio@gmail.com>
2022-04-13 14:54:40 +0900

    delete

--- src/main/java/com/test/git/model/User.java
+++ src/main/java/com/test/git/model/User.java
@@ -18,20 +18,8 @@ public class User {
     private Long userid;
 
 
-    @Column(nullable = false)
-    private String username;
 
-    @Column(nullable = false)
-    private String password;
 
-    @Column
-    private String nickname;
-
-    public User(String username, String password, String nickname){
-        this.username = username;
-        this.password = password;
-        this.nickname = nickname;
-    }
 
 
 }
